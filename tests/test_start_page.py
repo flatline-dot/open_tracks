9d2f925a55d5504aaab100bc9d5e888c73cfd205
stolypinskij_gv <stolypinskij_gv@NAVEL-048>
2022-05-24 14:14:54 +0300

    tests startpage

--- tests/test_start_page.py
+++ tests/test_start_page.py
@@ -1,7 +1,16 @@
 import app.create_app as app
+from time import sleep
 
 
 def test_check_con():
     application = app.App()
+    
     app.StartPage.check_con()
+    app.StartPage.psi_mode()
+    
     assert app.comports_status['COM2']['active']
+    assert app.comports_status['COM2']['psi_mode']
+    assert app.comports_status['COM2']['serial_instance'].port == "COM2"
+    sleep(0.5)
+    app.StartPage.default_set()
+    assert not app.comports_status['COM2']['psi_mode']
