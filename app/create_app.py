4734ba91813e160eee4661fb48459a6a28760ed1
stolypinskij_gv <stolypinskij_gv@NAVEL-048>
2022-05-30 20:14:35 +0300

    check bugs vector page

--- app/create_app.py
+++ app/create_app.py
@@ -60,6 +60,7 @@ class App(Tk):
         self.wm_state('zoomed')
         self['background'] = 'white'
         self.info_tracks_isrun = False
+        self.vector_status_isrun = False
 
         container = Frame(self, background='white')
         container.pack(side='top', fill='both', expand=True)
@@ -72,7 +73,7 @@ class App(Tk):
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky='nsew')
 
-        self.show_frame(VectorPage)
+        self.show_frame(StartPage)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
@@ -80,9 +81,11 @@ class App(Tk):
 
 
 class StartPage(Frame):
+    perm_buttons = []
     def __init__(self, parent, controller, comports_status):
         super().__init__(parent)
         self['background'] = 'white'
+        
 
         window_width = self.winfo_screenwidth()
         window_height = self.winfo_screenheight()
@@ -138,7 +141,7 @@ class StartPage(Frame):
         StartPage.render()
 
         class ButtonFrame(Frame):
-            def __init__(self, rely, text, command, container):
+            def __init__(self, rely, text, command, container, state='disabled'):
                 super().__init__(container)
                 self['width'] = button_frame_width
                 self['height'] = button_frame_height
@@ -149,15 +152,21 @@ class StartPage(Frame):
                 self.place(relx=0.5, rely=rely, anchor='center')
                 self.grid_propagate(False)
 
-                button = Button(self, text=text, width=22, height=2, borderwidth=2, font='Times 8 bold', relief='raised', command=command)
-                button.place(relx=0.5, rely=0.5, anchor='center')
-                button.grid_propagate(False)
+                self.button = Button(self, text=text, width=22, height=2, borderwidth=2, font='Times 8 bold', relief='raised', command=command, state=state)
+                self.button.place(relx=0.5, rely=0.5, anchor='center')
+                self.button.grid_propagate(False)
 
-        check_button = ButtonFrame(0.1, 'Проверка соединения', StartPage.check_con, container_buttons)
+        check_button = ButtonFrame(0.1, 'Установить соединение', StartPage.check_con, container_buttons, state='normal')
         psi_button = ButtonFrame(0.3, 'Режим ПСИ', StartPage.psi_mode, container_buttons)
         cannals_button = ButtonFrame(0.5, 'Инф. о каналах', lambda: controller.show_frame(InfoTracks), container_buttons)
-        vector_button = ButtonFrame(0.7, 'Вектор состояния', None, container_buttons)
+        vector_button = ButtonFrame(0.7, 'Вектор состояния', lambda: controller.show_frame(VectorPage), container_buttons)
         default_button = ButtonFrame(0.9, 'Настройки по умолчанию', StartPage.default_set, container_buttons)
+
+        self.perm_buttons.append(psi_button)
+        self.perm_buttons.append(cannals_button)
+        self.perm_buttons.append(vector_button)
+        self.perm_buttons.append(default_button)
+
 
     @staticmethod
     def check_con():
@@ -194,6 +203,8 @@ class StartPage(Frame):
                 comports_status[com.port]['active'] = True
 
         StartPage.render()
+        for frame in StartPage.perm_buttons:
+            frame.button['state'] = 'normal'
         return instance_ports
 
     @staticmethod
@@ -342,7 +353,6 @@ class InfoTracks(Frame):
 
     @staticmethod
     def info_tracks_parse(com_response):
-        #print(com_response)
         com, response = com_response
         if response:
             code_list = []
@@ -480,6 +490,8 @@ class InfoTracks(Frame):
 
 
 class VectorPage(Frame):
+    vectortables = {}
+
     def __init__(self, parent, controller, comports_status):
         super().__init__(parent)
         self['background'] = 'white'
@@ -493,7 +505,7 @@ class VectorPage(Frame):
         container_ports.grid(row=0, column=2, padx=(window_width / 10, 0))
         container_ports.grid_propagate(False)
 
-        class PortFrame(Frame):
+        class VectorTable(Frame):
             def __init__(self, title, container, *args, **kwargs):
                 super().__init__(container)
                 self['width'] = window_width / 12
@@ -501,6 +513,7 @@ class VectorPage(Frame):
                 self['borderwidth'] = 0
                 self['background'] = 'white'
                 self.grid_propagate(False)
+                self.restart_status = False
 
                 self.title_frame = Frame(self, width=self['width'], height=self['height'] / 4, borderwidth=1, relief='solid', background='white')
                 self.title_frame.grid(row=0, column=0)
@@ -517,51 +530,179 @@ class VectorPage(Frame):
                 self.time_frame = Frame(self, width=self['width'], height=self['height'] / 4, borderwidth=1, relief='solid', background='white')
                 self.time_frame.grid(row=2, column=0)
                 self.time_frame.grid_propagate(False)
-                self.time_label = Label(self.time_frame, text='Таймер', font='Times 14', background='white')
-                self.time_label.place(relx=0.5, rely=0.5, anchor='center')
+                self.totaltime_label = Label(self.time_frame, text='00:00:00', font='Times 12', background='white')
+                self.totaltime_label.place(relx=0.5, rely=0.5, anchor='center')
 
-                self.restart_frame = Frame(self, width=self['width'], height=self['height'] / 4, borderwidth=1, relief='solid', background='white')
+                self.restart_frame = Frame(self, width=self['width'], height=self['height'] / 5, borderwidth=1, relief='solid', background='white')
                 self.restart_frame.grid(row=3, column=0)
                 self.restart_frame.grid_propagate(False)
-                self.restrt_label = Label(self.restart_frame, text='Restart', font='Times 11', background='white')
-                self.restrt_label.place(relx=0.5, rely=0.5, anchor='center')
+
+                self.restart_button = Button(self.restart_frame, text='Restart', borderwidth=2, font='Cambria 8 bold', width=6, command=self.reverse_restart_flag, background='silver')
+                self.restart_button.place(rely=0.5, relx=0.5, anchor='center')
+                self.restart_button.grid_propagate(False)
+
+                VectorPage.vectortables[title] = self
+
+            def reverse_restart_flag(self):
+                self.restart_status = True
 
         high_row = ['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8']
         low_row = ['COM9', 'COM10', 'COM11', 'COM12', 'COM13', 'COM14', 'COM15', 'COM16']
 
         for title in high_row:
-            frame = PortFrame(title, container_ports)
-            ports_frames[title] = frame
+            frame = VectorTable(title, container_ports)
             frame.grid(row=0, column=high_row.index(title), padx=(0, 30), pady=(70, 50))
 
         for title in low_row:
-            frame = PortFrame(title, container_ports)
-            ports_frames[title] = frame
+            frame = VectorTable(title, container_ports)
             frame.grid(row=1, column=low_row.index(title), padx=(0, 30), pady=(0, 50))
 
-        buttonframe_width = window_width / 2
+        buttonframe_width = window_width / 1.5
         buttonframe_height = window_height / 12
         buttonframe = Frame(self, width=buttonframe_width, height=buttonframe_height, background='white', borderwidth=1)
         buttonframe.grid(columnspan=20)
         buttonframe.grid_propagate(False)
 
-        frame_start = Frame(buttonframe, width=buttonframe_width / 7, height=buttonframe_height / 2, background='white')
-        frame_start.place(relx=0.1, rely=0.7, anchor='center')
+        frame_start = Frame(buttonframe, width=buttonframe_width / 8, height=buttonframe_height / 2, background='white')
+        frame_start.place(relx=0.3, rely=0.7, anchor='center')
         frame_start.grid_propagate(False)
-        self.start_button = Button(frame_start, text='Старт', font='Arial 8 bold', borderwidth=2, background='silver', width=10, height=2, fg='black', command=None, relief='raised')
+        self.start_button = Button(frame_start, text='Старт', font='Arial 8 bold', borderwidth=2, background='silver', width=10, height=2, fg='black', command=self.start, relief='raised')
         self.start_button.place(relx=0.5, rely=0.5, anchor='center')
 
         frame_stop = Frame(buttonframe, width=buttonframe_width / 8, height=buttonframe_height / 2, background='white')
-        frame_stop.place(relx=0.3, rely=0.7, anchor='center')
+        frame_stop.place(relx=0.4, rely=0.7, anchor='center')
         frame_stop.grid_propagate(False)
-        self.stop_button = Button(frame_stop, text='Стоп', font='Arial 8 bold', borderwidth=2, background='silver', width=10, height=2, fg='black', command=None, state='disabled', relief='raised')
+        self.stop_button = Button(frame_stop, text='Стоп', font='Arial 8 bold', borderwidth=2, background='silver', width=10, height=2, fg='black', command=self.stop, state='disabled', relief='raised')
         self.stop_button.place(relx=0.5, rely=0.5, anchor='center')
 
-        frame_restart = Frame(buttonframe, width=buttonframe_width / 6, height=buttonframe_height / 2, background='white')
-        frame_restart.place(relx=0.5, rely=0.7, anchor='center')
-        frame_stop.grid_propagate(False)
-        self.restart_button = Button(frame_restart, text='Холодный перезапуск', font='Arial 8 bold', borderwidth=2, background='silver', width=20, height=2, fg='black', command=None, state='disabled', relief='raised')
+        frame_restart = Frame(buttonframe, width=buttonframe_width / 8, height=buttonframe_height / 2, background='white')
+        frame_restart.place(relx=0.53, rely=0.7, anchor='center')
+        frame_restart.grid_propagate(False)
+        self.restart_button = Button(frame_restart, text='Холодный перезапуск', font='Arial 8 bold', borderwidth=2, background='silver', width=20, height=2, fg='black', command=VectorPage.restart, state='disabled', relief='raised')
         self.restart_button.place(relx=0.5, rely=0.5, anchor='center')
+
+        backframe = Frame(buttonframe, width=buttonframe_width / 8, height=buttonframe_height / 2, background='white')
+        backframe.place(relx=0.1, rely=0.7, anchor='center')
+        backframe.grid_propagate(False)
+        self.back_button = Button(backframe, text='Назад', font='Arial 8 bold', borderwidth=2, background='silver', width=15, height=2, fg='black', command=lambda: controller.show_frame(StartPage), relief='raised')
+        self.back_button.place(relx=0.5, rely=0.5, anchor='center')
+
+
+    @staticmethod
+    def vector_read(com):
+        response = b''
+        if not com.warm_request:
+            if com.in_waiting >= 74:
+                response += com.read(com.in_waiting)
+                if (response[-1] == 3) and (response[-2] == 16) and (response[-3] != 16):
+                    com.reset_input_buffer()
+                    return (com, response)
+                else:
+                    return (com, None)
+            else:
+                return (com, None)
+        elif com.warm_request and ((time() - com.warm_restart_start) > 3):
+            #sleep(3)
+            com.warm_restart_start = True
+            table_port = VectorPage.vectortables[com.port]
+            table_port.start_time = time()
+            com.write(commands['vector_request'])
+            com.warm_request = False
+            table_port.restart_button['state'] = 'normal'
+            return (com, None)
+        else:
+            return (com, None)
+
+    @staticmethod
+    def vector_parse(com_response):
+        com, response = com_response
+        vector_status = False
+        if response:
+            response_clear = response.replace(bytes.fromhex('10') + bytes.fromhex('10'), bytes.fromhex('10'))
+            vector_status = int(response_clear[70:71].hex(), 16)
+        else:
+            return(com, False)
+        if vector_status == 17 or vector_status == 25:
+            return (com, True)
+        else:
+            return(com, False)
+
+    def vector_rendering(self, com_results):
+        com, result = com_results
+        table_port = VectorPage.vectortables[com.port]
+        if table_port.start_time == 'Restart':
+            table_port.totaltime_label['text'] = 'Restart'
+        else:
+            current_time = int(time() - table_port.start_time)
+            if current_time <= 36:
+                table_port.totaltime_label['text'] = f'00:00:{ current_time }'
+
+        if com.vector_status:
+            return None
+        else:
+            if result:
+                table_port.vector_frame['background'] = 'green'
+                table_port.vector_label['background'] = 'green'
+                com.vector_status = True
+            else:
+                table_port.vector_frame['background'] = 'red'
+                table_port.vector_label['background'] = 'red'
+
+        if table_port.restart_status:
+            com.write(commands['stop_request'])
+            com.write(commands['warm_restart'])
+            com.warm_restart_start = time()
+            table_port.start_time = 'Restart'
+            table_port.restart_status = False
+            table_port.restart_button['state'] = 'disabled'
+            com.warm_request = True
+            com.vector_status = False
+
+    @staticmethod
+    def restart():
+        worker_ports = [comports_status[com]['serial_instance'] for com in comports_status if comports_status[com]['active']]
+        for com in worker_ports:
+            com.vector_status = False
+            com.write(commands['warm_restart'])
+        sleep(3)
+        for com in worker_ports:
+            com.write(commands['vector_request'])
+        for table_port in VectorPage.vectortables:
+            table_port = VectorPage.vectortables[table_port]
+            table_port.start_time = time()
+
+    def start(self):
+        App.vector_status_isrun = True
+        self.back_button['state'] = 'disabled'
+        self.start_button['state'] = 'disabled'
+        self.restart_button['state'] = 'normal'
+        self.stop_button['state'] = 'normal'
+        ready_ports = [comports_status[com]['serial_instance'] for com in comports_status if comports_status[com]['active']]
+
+        [port.write(commands['vector_request']) for port in ready_ports]
+        for table in VectorPage.vectortables:
+            table_port = VectorPage.vectortables[table]
+            table_port.start_time = time()
+
+        def running():
+            if App.vector_status_isrun:
+                read_results = [VectorPage.vector_read(port) for port in ready_ports]
+                parse_results = [VectorPage.vector_parse(port) for port in read_results]
+                [self.vector_rendering(port) for port in parse_results]
+                Tk.after(app, 100, running)
+
+        running()
+
+    def stop(self):
+        self.back_button['state'] = 'normal'
+        self.start_button['state'] = 'normal'
+        self.restart_button['state'] = 'disabled'
+        self.stop_button['state'] = 'disabled'
+        App.vector_status_isrun = False
+        ready_ports = [comports_status[com]['serial_instance'] for com in comports_status if comports_status[com]['active']]
+        [port.write(commands['stop_request']) for port in ready_ports]
+        sleep(0.5)
+        [port.reset_input_buffer() for port in ready_ports]
 
 
 if __name__ == '__main__':
